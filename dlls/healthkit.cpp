92f16acbb54fcf32c62806c8cd48db4d4c733ae0
Deskarpiostin <georgijcerenkov074@gmail.com>
2025-12-29 09:36:54 +0800

    first commit

---
+++ dlls/healthkit.cpp
@@ -0,0 +1,94 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "monsters.h"
+#include "weapons.h"
+#include "nodes.h"
+#include "player.h"
+#include "items.h"
+#include "gamerules.h"
+#include "game.h"
+
+extern int gmsgItemPickup;
+
+class CHealthKit : public CItem
+{
+	void Spawn( void );
+	void Precache( void );
+	BOOL MyTouch( CBasePlayer *pPlayer );
+/*
+	virtual int Save( CSave &save ); 
+	virtual int Restore( CRestore &restore );
+	static TYPEDESCRIPTION m_SaveData[];
+*/
+};
+
+LINK_ENTITY_TO_CLASS( item_healthkit, CHealthKit )
+
+/*
+TYPEDESCRIPTION	CHealthKit::m_SaveData[] =
+{
+
+};
+
+IMPLEMENT_SAVERESTORE( CHealthKit, CItem )
+*/
+
+void CHealthKit::Spawn( void )
+{
+	Precache();
+	SET_MODEL( ENT( pev ), "models/w_medkit.mdl" );
+
+	CItem::Spawn();
+}
+
+void CHealthKit::Precache( void )
+{
+	PRECACHE_MODEL( "models/w_medkit.mdl" );
+	PRECACHE_SOUND( "items/smallmedkit1.wav" );
+}
+
+BOOL CHealthKit::MyTouch( CBasePlayer *pPlayer )
+{
+	if( pPlayer->pev->deadflag != DEAD_NO )
+	{
+		return FALSE;
+	}
+
+	if( pPlayer->TakeHealth( gSkillData.healthkitCapacity, DMG_GENERIC ) )
+	{
+		MESSAGE_BEGIN( MSG_ONE, gmsgItemPickup, NULL, pPlayer->pev );
+			WRITE_STRING( STRING( pev->classname ) );
+		MESSAGE_END();
+
+		EMIT_SOUND( ENT( pPlayer->pev ), CHAN_ITEM, "items/smallmedkit1.wav", 1, ATTN_NORM );
+
+		if( g_pGameRules->ItemShouldRespawn( this ) )
+		{
+			Respawn();
+		}
+		else
+		{
+			UTIL_Remove( this );	
+		}
+
+		return TRUE;
+	}
+
+	return FALSE;
+}
